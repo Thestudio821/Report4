7fc6539fc1c2a3e2b56eb4cad827a65bfb8ef964
Kazunari ENOKAWA <e195720@ie.u-ryukyu.ac.jp>
2019-11-26 11:01:28 +0900

    java.lang.NumberFormatException

--- src/main/Task.java
+++ src/main/Task.java
@@ -1,5 +1,7 @@
+//https://github.com/Thestudio821/Report4.git
 public class Task {
     public static void main(String[] args){
+        /**
         String str = null;
         try {
             System.out.print(str.length());
@@ -8,6 +10,20 @@ public class Task {
             System.out.println(e.getMessage());
             System.out.println(e.getClass());
             System.out.println("想定外のことが発生しました。ご確認ください");
+        }
+    }
+         }
+*/
+        String str = "百十三";
+        int value;
+
+        try {
+            value = Integer.parseInt(str);
+            System.out.println(value);
+        }catch (Exception e){
+            System.out.println(e.getClass());
+            System.out.println(e.getMessage());
+            System.out.println("想定外のことが発生しました。");
         }
     }
 }
